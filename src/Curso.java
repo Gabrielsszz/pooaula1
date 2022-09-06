d2334e460e6646b631fe4bc5997c3ee23f601bb5
Wesley <wesleyvc2110@gmail.com>
2022-09-05 21:33:28 -0300

    first commit

---
+++ src/Curso.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+
+public class Curso {
+    
+        int cargaHoraria;
+        int nivel;
+        String nomeDoCurso;
+        String modalidade;
+        LocalDate dataDeInicio;
+        LocalDate dataDeTermino;
+        Boolean gratuito;
+        
+
+
+
+    
+
+}
